d28a01caec5e4ab7d41e30438b40e30773fbf4c4
jonathanramos <jonathaniramos360@gmail.com>
2025-02-20 13:15:55 -0500

    fixing firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCPPHAHTMNAqKXi6tkbbPCl5tnbBn8EZq8",
-  authDomain: "the-detailers-edge.firebaseapp.com",
-  databaseURL: "https://the-detailers-edge-default-rtdb.firebaseio.com",
-  projectId: "the-detailers-edge",
-  storageBucket: "the-detailers-edge.firebasestorage.app",
-  messagingSenderId: "964635821212",
-  appId: "1:964635821212:web:f535c7a1d910c0d1a51959",
-  measurementId: "G-0BZHMF4RP5"
+const firebaseConfig = { /* import.meta.env = process.env -> used because of vite */
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
