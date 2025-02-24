8e9ca69929c85df6c8ce9419476b4aef0c9746ec
AYOPOSI O <aolu-bamisaye@loyola.edu>
2025-02-24 15:34:05 -0500

    added new api to handle reschduling, reminders

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -76,7 +76,8 @@ const Booking = () => {
           </div>
         </div>
     
-            <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1apZgQhXcZuUB6IMKN5usRC297BwibW1z8aOUOxNPhMmJ9Xy5gUihajbJ-bz3XlSkFM6TmHY3P?gv=true" 
+            <iframe src= "https://ayoposi.neetocal.com/meeting-with-detailers-edge"
+            //"https://calendar.google.com/calendar/appointments/schedules/AcZssZ1apZgQhXcZuUB6IMKN5usRC297BwibW1z8aOUOxNPhMmJ9Xy5gUihajbJ-bz3XlSkFM6TmHY3P?gv=true" 
                     style={{border: 0}} 
                     width="100%" 
                     height="600" 
