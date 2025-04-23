b0f03a7e993125c2b07aa473dd6a6b35ee7dd0e7
AYOPOSI O <aolu-bamisaye@loyola.edu>
2025-04-22 20:20:49 -0400

    added jest config for backend

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,1 +1,14 @@
- 
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  verbose: true,
+  setupFilesAfterEnv: ['./src/tests/setup.js'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/tests/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
+}; 
